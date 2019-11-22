7994669f79798d5186579fc8c722fced0386edc7
Aleksey Zorin <azegence@gmail.com>
2019-11-22 16:30:25 +0100

    Сделал в мастере логгирование в файл

--- configs/config.php
+++ configs/config.php
@@ -3,16 +3,20 @@
 use Monolog\Logger;
 use Monolog\Handler\ErrorLogHandler;
 use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
 
 $stdoutLoggerHandler = new ErrorLogHandler();
 $stdoutLoggerHandler->setFormatter(
     new LineFormatter(">>> %message% (%level_name% | %datetime%) %context% %extra%\n", 'Y.m.d H:i:s')
 );
 
+$fileHandler = new StreamHandler(__DIR__ . '/../../ws.error.log', Logger::ERROR);
+
 return [
     'port' => 25025,
     'backendAddr' => 'http://localhost:8080/',
     'logger' => new Logger('PhpWebSocketApplication', [
         $stdoutLoggerHandler,
+        $fileHandler,
     ]),
 ];
